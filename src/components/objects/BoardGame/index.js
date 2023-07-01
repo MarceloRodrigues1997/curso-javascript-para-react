3efb704d39facf574d9508d5a37b38d5d5e98d84
Marcelo R <marcelorodriguesjs1@g,mail.com>
2023-07-01 06:35:02 -0300

    OK

--- src/components/objects/BoardGame/index.js
+++ src/components/objects/BoardGame/index.js
@@ -1,10 +1,15 @@
 import CardGame from "../../CardGame";
+import "./style.css";
 
 function BoardGame(amountCards) {
   const $htmlCardGame = CardGame();
   const $htmlBoardGame = $htmlCardGame.repeat(amountCards);
 
-  return $htmlBoardGame;
+  return `
+  <section class=board-game>
+  ${$htmlBoardGame}
+  </section>
+  `;
 }
 
 export default BoardGame;
